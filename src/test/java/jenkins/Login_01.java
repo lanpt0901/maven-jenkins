9a8224dd5841e03b3c067bb92d8a6d228076d65a
Pham Lan <lan.phamthi@ascendcorp.com>
2022-02-26 21:56:48 +0700

    Init

---
+++ src/test/java/jenkins/Login_01.java
@@ -0,0 +1,51 @@
+package jenkins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login_01 {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+//		driver = new FirefoxDriver();
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		driver.manage().window().maximize();
+//		driver.get("http://demo.guru99.com/v4/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+//		driver.quit();
+	}
+
+	@Test
+	public void TC_01_CreateNewAccount() {
+	}
+
+	@Test
+	public void TC_02_Login() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void TC_03_TestMenu() {
+	}
+	
+}
